f8593d3c4674f8fe22e09dfcf02174c0c5ea75a6
Gabriel Oliveira Silva <gabriel.oliveira@programmers.com.br>
2022-05-16 10:44:08 -0300

    Exercícios iniciais

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,6 @@
+﻿double cotacaoEuro = 5.27;
+float valorEuro = 750;
+double conversaoReal = valorEuro * cotacaoEuro;
+
+Console.WriteLine("A compra de 750 euros covertida para real é igual a {0}", conversaoReal);
+
---
+++ Ex3/Ex3/Program.cs
@@ -0,0 +1,5 @@
+﻿int totalVenda = 500000;
+double valorComissao = 0.05;
+double totalComissao = totalVenda * valorComissao;
+
+Console.WriteLine("Ele receberá de comissão: {0}",totalComissao);
