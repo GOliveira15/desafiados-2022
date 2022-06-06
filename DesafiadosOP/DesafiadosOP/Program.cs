7295283edc83e0ad7399b39fdb3f60a23f6255c7
Gabriel Oliveira Silva <gabriel.oliveira@programmers.com.br>
2022-06-06 10:51:35 -0300

    Operadores

---
+++ DesafiadosOP/DesafiadosOP/Program.cs
@@ -0,0 +1,10 @@
+﻿double media;
+
+Console.Write("Informe a média do aluno: ");
+media = double.Parse(Console.ReadLine());
+
+var status = media > 7 ? "Aprovado" : "Reprovado";
+
+//var status = (media >= 8 ? "Aprovado" : ((media < 8 && media >= 5) ? "Recuperação" : "Reprovado"));
+
+Console.Write(status);
